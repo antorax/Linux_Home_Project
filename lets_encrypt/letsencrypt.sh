f3e89be5be25245b5332e54559c8486f53b78fdd
Jelle Van Hees <jellev@aviovision.com>
2016-10-07 11:29:36 +0200

    first work(unfinished)

---
+++ lets_encrypt/letsencrypt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $FIRST_RUN ];
+    then
+        # certbot --apache -m $LETSEMAIL -d $LETSDOMAIN --agree-tos certonly --quiet
+        echo "first"
+
+    else
+        #certbot -m $LETSEMAIL -d $LETSDOMAIN --agree-tos renew --quiet
+        echo "renew"
+fi
+
