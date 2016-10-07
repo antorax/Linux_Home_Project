70514ff5d70af0ed458b63ca8b40218b7f48ae7e
Jelle Van Hees <jellev@aviovision.com>
2016-10-07 17:13:28 +0200

    last changes befor going home

--- lets_encrypt/letsencrypt.sh
+++ lets_encrypt/letsencrypt.sh
@@ -2,11 +2,11 @@
 
 if [ $FIRST_RUN ];
     then
-        # certbot --apache -m $LETSEMAIL -d $LETSDOMAIN --agree-tos certonly --quiet
-        echo "first"
+        certbot --apache -m $LETSEMAIL -d $LETSDOMAIN --agree-tos certonly --quiet
+        echo "First run"
 
     else
-        #certbot -m $LETSEMAIL -d $LETSDOMAIN --agree-tos renew --quiet
+        certbot -m $LETSEMAIL -d $LETSDOMAIN --agree-tos renew --quiet
         echo "renew"
 fi
 
